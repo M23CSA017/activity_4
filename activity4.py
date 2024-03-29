e87a4f12e90c5ad8acd6ef77aafa351201f32fc2
M23CSA017 <m23csa017@iitj.ac.in>
2024-03-30 01:53:33 +0530

    version-1

---
+++ activity4.py
@@ -0,0 +1,109 @@
+import torch
+import torchvision
+from torchvision import transforms, datasets
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader, Dataset
+from torchvision import datasets, transforms
+from sklearn.metrics import accuracy_score, precision_score, recall_score, confusion_matrix
+import numpy as np
+
+class USPSDataset(Dataset):
+    def __init__(self, data, target, transform=None):
+        self.data = data
+        self.target = target
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        img = self.data[idx]
+        label = self.target[idx]
+
+        if self.transform:
+            img = self.transform(img)
+
+        return img, label
+
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Conv2d(1, 16, 3, padding=1)
+        self.conv2 = nn.Conv2d(16, 32, 3, padding=1)
+        self.conv3 = nn.Conv2d(32, 64, 3, padding=1)
+        self.fc1 = nn.Linear(256, 128)
+        self.fc2 = nn.Linear(128, 10)
+
+    def forward(self, x):
+        x = torch.relu(self.conv1(x))
+        x = torch.max_pool2d(x, 2)
+        x = torch.relu(self.conv2(x))
+        x = torch.max_pool2d(x, 2)
+        x = torch.relu(self.conv3(x))
+        x = torch.max_pool2d(x, 2)
+        x = torch.flatten(x, 1)
+        x = torch.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+def train_model(model, train_loader, criterion, optimizer, epoch):
+    model.train()
+    running_loss = 0.0
+    for batch_idx, (data, target) in enumerate(train_loader):
+        optimizer.zero_grad()
+        output = model(data)
+        loss = criterion(output, target)
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.item()
+        if batch_idx % 100 == 99:
+            print('[%d, %5d] loss: %.3f' %
+                  (epoch + 1, batch_idx + 1, running_loss / 100))
+            running_loss = 0.0
+    return running_loss / len(train_loader)
+
+def evaluate_model(model, test_loader):
+    model.eval()
+    y_true = []
+    y_pred = []
+    with torch.no_grad():
+        for data, target in test_loader:
+            output = model(data)
+            _, predicted = torch.max(output, 1)
+            y_true.extend(target.tolist())
+            y_pred.extend(predicted.tolist())
+    accuracy = accuracy_score(y_true, y_pred)
+    precision = precision_score(y_true, y_pred, average='weighted')
+    recall = recall_score(y_true, y_pred, average='weighted')
+    cm = confusion_matrix(y_true, y_pred)
+    return accuracy, precision, recall, cm
+
+transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
+
+train_data = datasets.USPS(root='./data', train=True, download=True, transform=transforms.ToTensor())
+test_data = datasets.USPS(root='./data', train=False, download=True, transform=transforms.ToTensor())
+
+batch_size = 32
+train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
+test_loader = DataLoader(test_data, batch_size=batch_size, shuffle=False)
+
+cnn_model = CNN()
+lr = 0.001
+cnn_optimizer = optim.Adam(cnn_model.parameters(), lr=lr)
+
+
+criterion = nn.CrossEntropyLoss()
+
+epochs = 5
+
+for epoch in range(epochs):
+    print(f"Epoch {epoch + 1}/{epochs}")
+    cnn_loss = train_model(cnn_model, train_loader, criterion, cnn_optimizer, epoch)
+    print("CNN Training Loss: {:.4f}".format(cnn_loss))
+    
+    cnn_acc, cnn_prec, cnn_recall, cnn_cm = evaluate_model(cnn_model, test_loader)
+    print("CNN Test Accuracy: {:.4f}, Precision: {:.4f}, Recall: {:.4f}".format(cnn_acc, cnn_prec, cnn_recall))
+    print("Confusion Matrix:")
+    print(cnn_cm)
+    print("")
